72b4fed02eb13642023e5c82cb7997a751c9d51c
weeeeey <qser155@naver.com>
2022-11-16 16:30:18 +0900

    cf) _app 컴포넌트 파라미터 타입 지정 AppProps

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,9 @@
+import type { AppProps } from "next/app";
+
 import Navbar from "../components/Navbar";
 
-const Myapp = ({ Component, pageProps }) => {
+const Myapp = ({ Component, pageProps }: AppProps) => {
+    console.log(Component);
     return (
         <>
             <Navbar /> <Component {...pageProps} />
