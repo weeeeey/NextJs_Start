507670b1892f0f9b88f36994e3248265ad998986
weeeeey <qser155@naver.com>
2022-11-16 20:00:10 +0900

    #2.1 redirects 를 통해 url 강제 변경
    next.confing.js 에 redirects 함수를 통해 url 변경

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -4,46 +4,44 @@ import { useRouter } from "next/router";
 const Navbar = () => {
     const router = useRouter();
     return (
-        <div>
-            <nav>
-                <Link href={"/"} legacyBehavior>
-                    <a className={router.pathname == "/" ? "active" : ""}>
-                        Home{" "}
-                    </a>
-                </Link>
-                <Link legacyBehavior href={"/about"}>
-                    <a className={router.pathname == "/about" ? "active" : ""}>
-                        About
-                    </a>
-                </Link>
-                <style jsx>{`
-                    nav {
+        <div className="Nav">
+            <Link legacyBehavior href={"/"}>
+                <a className={router.pathname == "/about" ? "active" : ""}>
+                    Home
+                </a>
+            </Link>
+            <Link legacyBehavior href={"/about"}>
+                <a className={router.pathname == "/about" ? "active" : ""}>
+                    About
+                </a>
+            </Link>
+            <style jsx>{`
+                .Nav {
+                    display: flex;
+                    gap: 10px;
+                    flex-direction: column;
+                    align-items: center;
+                    padding-top: 20px;
+                    padding-bottom: 10px;
+                    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+                    img {
+                        max-width: 100px;
+                        margin-bottom: 5px;
+                    }
+                    nav a {
+                        font-weight: 600;
+                        font-size: 18px;
+                    }
+                    .active {
+                        color: tomato;
+                    }
+                    nav div {
                         display: flex;
                         gap: 10px;
-                        flex-direction: column;
-                        align-items: center;
-                        padding-top: 20px;
-                        padding-bottom: 10px;
-                        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-                            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-                        img {
-                            max-width: 100px;
-                            margin-bottom: 5px;
-                        }
-                        nav a {
-                            font-weight: 600;
-                            font-size: 18px;
-                        }
-                        .active {
-                            color: tomato;
-                        }
-                        nav div {
-                            display: flex;
-                            gap: 10px;
-                        }
                     }
-                `}</style>
-            </nav>
+                }
+            `}</style>
         </div>
     );
 };
--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: "/old",
+                destination: "https:www.naver.com",
+                permanent: false,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
