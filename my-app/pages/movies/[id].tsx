0fd314257221dc7fa7ea09f22337c4019781b1d0
weeeeey <qser155@naver.com>
2022-11-17 19:07:37 +0900

    #2.5 Dynamic Routes // movies/:id ==> movies 폴더 안에 [id].tsx 생성

---
+++ my-app/pages/movies/[id].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+const MovieDetail = () => {
+    const route = useRouter();
+    console.log(route);
+    return "movie detail";
+};
+
+export default MovieDetail;
+
+// dynamic Route 를 다룰때는 (/movies/1234) 이럴 경우 movies라는 폴더 안에
+// [id].tsx 를 적어주면 됨
+// id.tsx라면 그건 그냥 movies/id 라는 경로일뿐
